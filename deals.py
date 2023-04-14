9cfd3a52829b5588ea4aa5dbf789d2a0ce2fe102
AmDizzy <abdojoh@hotmail.com>
2023-04-14 03:10:39 +0200

    first commit

---
+++ deals.py
@@ -0,0 +1,52 @@
+from discord.ext import tasks, commands
+import asyncio, logging
+from scrapers import Scrapers
+
+class DealsCog(commands.Cog):
+    def __init__(self, bot):
+        self.logger = logging.getLogger('discord')
+        self.bot = bot
+        self.scrapers = Scrapers()
+        self.scrape.start()
+
+    def cog_unload(self):
+        self.logger.info('DealsCog unloading')
+        self.scrape.cancel()
+
+    @tasks.loop(seconds=5)
+    async def scrape(self):
+        self.logger.info('DealsCog started')
+        try:
+            adealsweden = self.scrapers.scrape_adealsweden()
+            if self.scrapers.adealsweden_old:
+                for channel in self.bot.allowed_channels:
+                    for adeal in adealsweden:
+                        await channel.send(content=f'@everyone new deal from adealsweden.com\n{adeal.name}\n{adeal.price}\n{adeal.url}')
+                        await asyncio.sleep(2)
+        except:
+            self.logger.error(f'Failed to scrape: adealsweden.com')
+        try:
+            swedroid = self.scrapers.scrape_swedroid()
+            if self.scrapers.swedroid_old:
+                for channel in self.bot.allowed_channels:
+                    for droid in swedroid:
+                        url = droid.url.split('?')[0]
+                        await channel.send(content=f'@everyone new deal from swedroid.se\n{url}')
+                        await asyncio.sleep(2)
+        except:
+            self.logger.error(f'Failed to scrape: swedroid.se')
+        try:
+            amazon = self.scrapers.scrape_amazon()
+            if self.scrapers.amazon_old:
+                for channel in self.bot.allowed_channels:
+                    for amaz in amazon:
+                        print(amaz.url)
+                        await channel.send(content=f'@everyone product on amazon.se\n{amaz.name}\n{amaz.price}\n{amaz.url}')
+                        await asyncio.sleep(2)
+        except:
+            self.logger.error(f'Failed to scrape: amazon.se')
+
+    @scrape.before_loop
+    async def before_printer(self):
+        self.logger.info('DealsCog waiting...')
+        await self.bot.wait_until_ready()
---
+++ main.py
@@ -0,0 +1,61 @@
+from deals import DealsCog
+from discord.ext import commands
+import discord, configparser, os, logging
+from dotenv import load_dotenv
+
+CONFIG = 'config.ini'
+CONFIG_DISCORD = 'Discord'
+CONFIG_DISCORD_SERVERS = 'servers'
+CONFIG_DISCORD_CHANNELS = 'channels'
+LOG_FILE = 'discord.log'
+LOGGER = 'discord'
+TOKEN = 'TOKEN'
+
+# https://discordpy.readthedocs.io/en/stable/api.html
+
+class DealsBot(commands.Bot):
+    def __init__(self, servers, channels, logger, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.servers = servers
+        self.channels = channels
+        self.logger = logger
+        self.allowed_channels = []
+
+    async def on_ready(self):
+        self.logger.info(f'Logged in as {self.user}')
+        for guild in self.guilds:
+            if guild.name in self.servers: # check if whitelisted server
+                self.logger.info(f'Connected to server: {guild}')
+                for channel in guild.channels:
+                    for c in self.channels:
+                        if c in channel.name and channel.type.name == 'text': # check if whitelisted channel
+                            self.logger.info(f'Found channel for posting deals: {channel.name}')
+                            await channel.send('Successfully connected!')
+                            self.allowed_channels.append(channel)
+                            break
+        await self.add_cog(DealsCog(self))
+
+def main ():
+    logger = logging.getLogger(LOGGER)
+    load_dotenv()
+    config = configparser.ConfigParser()
+    servers = []
+    channels = []
+    try:
+        config.read(CONFIG)
+        ss = config.get(CONFIG_DISCORD, CONFIG_DISCORD_SERVERS).split(',')
+        for s in ss:
+            servers.append(s.strip())
+        cc = config.get(CONFIG_DISCORD, CONFIG_DISCORD_CHANNELS).split(',')
+        for c in cc:
+            channels.append(c.strip())
+    except:
+        logger.info('Failed to parse config.ini')
+        exit(1)
+    handler = logging.FileHandler(filename=LOG_FILE, encoding='utf-8', mode='a')
+    allowed_mentions = discord.AllowedMentions(everyone=True)
+    client = DealsBot(servers, channels, logger, intents=None, command_prefix='!', allowed_mentions=allowed_mentions)    
+    client.run(os.getenv(TOKEN), log_handler=handler, log_level=logging.INFO)
+
+if __name__ == "__main__":
+    main()
